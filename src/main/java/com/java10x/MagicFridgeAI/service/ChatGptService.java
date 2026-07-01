371250fc91ebf6cc9f4e8e1105b169bf72f899ea
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2026-07-01 20:18:36 -0300

    feat: cria service para fazer o metodo que vai configurar e gerar a resposta do prompt, definindo todos os parametros do CURL da OpenAi

---
+++ src/main/java/com/java10x/MagicFridgeAI/service/ChatGptService.java
@@ -0,0 +1,69 @@
+package com.java10x.MagicFridgeAI.service;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ChatGptService {
+
+    private final WebClient webClient;
+    private String apiKey = System.getenv("API_KEY");
+
+    public ChatGptService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    /*
+    * curl "https://api.openai.com/v1/responses" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $OPENAI_API_KEY" \
+    -d '{
+        "model": "gpt-5.5",
+        "reasoning": {"effort": "low"},
+        "input": [
+            {
+                "role": "developer",
+                "content": "Talk like a pirate."
+            },
+            {
+                "role": "user",
+                "content": "Are semicolons optional in JavaScript?"
+            }
+        ]
+    }'
+    * */
+    public Mono<String> generateRecipe() {
+        String prompt = "Me sugira uma receita simples com ingredientes comuns";
+        Map requestBody = Map.of(
+                "model", "gpt-5",
+                "reasoning", Map.of("effort", "low"), //define baixo esforço de raciocínio,reduz latência e custo
+                "input", List.of(
+                        Map.of("role", "system","content", "Você é um assistente que cria receitas"),
+                        Map.of("role", "user", "content", prompt)
+                )
+        );
+
+        return webClient.post()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(Map.class)
+                .map(response -> {
+                    var choices = (List<Map<String, Object>>) response.get("choices");
+                    if (choices != null && !choices.isEmpty()) {
+                        Map<String, Object> input = (Map<String, Object>) choices.get(0);
+                        return input.get("content").toString();
+                    }
+                    return "Nenhuma receita foi gerada.";
+                });
+    }
+
+}
